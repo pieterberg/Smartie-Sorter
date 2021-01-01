4918355d9d7f4d95084cfc9cc7261c35cc07cfb9
Pieter van den Berg <pieterleonvdb@outlook.com>
2021-01-01 12:54:52 +0200

    Created colour_calibration.ino

---
+++ smartie_sorter/colour_calibration.ino
@@ -0,0 +1,17 @@
+// Sketch for colour calibration of the Smarties for the Smartie Sorter
+// Created by Pieter van den Berg
+// on 1 January 2021
+//
+// Based on Skittles Sorter from How to Mechatronics
+// https://howtomechatronics.com/projects/arduino-color-sorter-project/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
