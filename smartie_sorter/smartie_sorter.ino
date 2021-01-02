708814fe8d28ce7c813157c554d73c479bbf8a78
Pieter van den Berg <pieterleonvdb@outlook.com>
2021-01-02 09:34:24 +0200

    Added reading the colour frequency

--- smartie_sorter/smartie_sorter.ino
+++ smartie_sorter/smartie_sorter.ino
@@ -11,6 +11,7 @@ const int colourSensorS0 = 6;
 const int colourSensorS1 = 5;
 const int colourSensorS2 = 4;
 const int colourSensorS3 = 3;
+const int colourSensorLED = 7;
 const int colourSensorOut = 2;
 
 
@@ -20,13 +21,30 @@ void setup() {
   pinMode(colourSensorS1, OUTPUT);
   pinMode(colourSensorS2, OUTPUT);
   pinMode(colourSensorS3, OUTPUT);
+  pinMode(colourSensorLED, OUTPUT);
   pinMode(colourSensorOut, INPUT);
 
+  // Set scaling frequency of colour sensor to 100%
+  digitalWrite(colourSensorS0, HIGH);
+  digitalWrite(colourSensorS1, HIGH);
 
+  digitalWrite(colourSensorLED, HIGH);
+
+  // Begin a serial channel
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 
+  Serial.print("red: ");
+  Serial.print(readRedValue());
+  Serial.print("  green: ");
+  Serial.print(readGreenValue());
+  Serial.print("  blue: ");
+  Serial.println(readBlueValue());
+
+  delay(100);
 
 }
 
@@ -34,16 +52,39 @@ void loop() {
 
 // Reads the red RGB value of the Smartie
 int readRedValue() {
+  int redFrequency;
 
+  // Set up the colour sensor's pins to read the red frequency value
+  digitalWrite(colourSensorS2, LOW);
+  digitalWrite(colourSensorS3, LOW);
+
+  // Read the red colour frequency
+  redFrequency = pulseIn(colourSensorOut, LOW);
+  return redFrequency;
 }
 
 // Reads the green RGB value of the Smartie
 int readGreenValue() {
+  int greenFrequency;
 
+  // Set up the colour sensor's pins to read the green frequency value
+  digitalWrite(colourSensorS2, HIGH);
+  digitalWrite(colourSensorS3,HIGH);
+
+  // Read the green colour frequency
+  greenFrequency = pulseIn(colourSensorOut, LOW);
+  return greenFrequency;
 }
 
 // Reads the blue RGB value of the Smartie
-int readBluevalue() {
+int readBlueValue() {
+  int blueFrequency;
 
+  // Set up the colour sensor's pins to read the blue frequency value
+  digitalWrite(colourSensorS2, LOW);
+  digitalWrite(colourSensorS3, HIGH);
+
+  // Read the blue colour frequency
+  blueFrequency = pulseIn(colourSensorOut, LOW);
+  return blueFrequency;
 }
-
