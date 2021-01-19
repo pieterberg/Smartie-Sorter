31d4fa0ed1595ac08e0ba0b804d9d5015e11ecbf
Pieter van den Berg <pieterleonvdb@outlook.com>
2021-01-19 09:05:19 +0200

    Added private members to class

--- smartie_sorter/TCS3200_colour_sensor.h
+++ smartie_sorter/TCS3200_colour_sensor.h
@@ -6,4 +6,25 @@ class TCS3200_colour_sensor
 {
     public:
         TCS3200_colour_sensor();
+
+
+
+    private:
+        // Colour Sensor's pins
+        int _colourSensorS0;
+        int _colourSensorS1;
+        int _colourSensorS2;
+        int _colourSensorS3;
+        int _colourSensorLED;
+        int _colourSensorOut;
+
+        // Calibrated frequencies for white and black
+        // Use the calibration function to determine these values
+        int _redWhiteFrequency;
+        int _greenWhiteFrequency;
+        int _blueWhiteFrequency;
+
+        int _redBlackFrequency;
+        int _greenBlackFrequency;
+        int _blueBlackFrequency;
 };
