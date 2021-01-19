7c2cd4affa381d4360866d5a6457a414a407b300
Pieter van den Berg <pieterleonvdb@outlook.com>
2021-01-19 09:21:34 +0200

    Added public functions to colour sensor class

--- smartie_sorter/TCS3200_colour_sensor.h
+++ smartie_sorter/TCS3200_colour_sensor.h
@@ -2,12 +2,28 @@
 
 #include <Arduino.h>
 
+// Library for the TCS3200 colour sensor component
 class TCS3200_colour_sensor
 {
     public:
-        TCS3200_colour_sensor();
+        // Constructor
+        TCS3200_colour_sensor(int S0, int S1, int S2, int S3, int LED, int OUT);
 
+        // Set frequency scaling for the colour sorter
+        void setFrequencyScalingToZero();
+        void setFrequencyScalingTo2Percent();
+        void setFrequencyScalingTo20Percent();
+        void setFrequencyScalingTo100Percent();
 
+        // Functions to read the frequency returned by the colour sensor
+        int readRedFrequency();
+        int readGreenFrequency();
+        int readBlueFrequency();
+
+        // Functions to read the RGB mapping of the colour read by the colour sensor
+        int readRedRGB();
+        int readGreenRGB();
+        int readBlueRGB();
 
     private:
         // Colour Sensor's pins
