d3598f66fe9d55eaccb6efdc086f610049a70f60
Pieter van den Berg <pieterleonvdb@outlook.com>
2025-07-14 09:03:07 +0200

    Created the calibration.ino sketch

---
+++ calibration/calibration.ino
@@ -0,0 +1,16 @@
+// Project name: Smartie Sorter 3000
+// Sketch name:  calibration.ino
+// Created by:   Pieter van den Berg
+// Created on:   14 July 2025
+
+// Code to calibrate and setup the Smartie Sorter 3000's mini arcade game enclosure
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
